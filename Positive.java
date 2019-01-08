05bd2bb0391acca76c768ad0583fc39a6b4474b1
Ramalinga Prasad G S <32306900+Ramalinga12Prasad@users.noreply.github.com>
2019-01-08 22:01:54 +0530

    Update Positive.java

--- Positive.java
+++ Positive.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package positive;
-import java.util.Scanner;
-/**
- *
- * @author Ram
- */
-public class Positive {
 
-    public static void main(String[] args) {
+
+import java.util.Scanner;
+public class Positive {
+ public static void main(String[] args) {
 Scanner in = new Scanner(System.in);
 int n = in.nextInt();
 if(n>0){
