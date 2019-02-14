c45b95ebfe2042e80a38a6fdfab624412a4a017a
Ramalinga12Prasad <rprprasath05@gmail.com>
2019-02-14 20:51:14 +0530

    Vowel.java file added

---
+++ vowel.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class vowel{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        String x = in.next();
+        if(x.length()>1){
+        
+        System.out.println("Invalid");
+        }
+    else{    
+        char a = x.charAt(0);
+        if(Character.isAlphabetic(a)){
+            if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
+                System.out.println("Vowel");
+            }
+            else{
+                System.out.println("Consonant");
+            }
+        }
+        else{
+            System.out.println("Invalid");
+        }
+    }
+
+    }
+}
