c2caa7f1703cf99e63dc078f41bcc89e9b6f8106
Ramalinga12Prasad <rprprasath05@gmail.com>
2019-02-15 15:40:55 +0530

    modified the palindrome for larger length

--- beginner/set-2/palindrome.java
+++ beginner/set-2/palindrome.java
@@ -2,25 +2,17 @@ import java.util.Scanner;
 public class palindrome{
     public static void main(String args[]) {
      Scanner in = new Scanner(System.in);
-     int n = in.nextInt();
-     int k = reverse(n);    
+    String x = in.next();
+    StringBuffer sb = new StringBuffer(x);
+     sb =sb.reverse();
      
-     if(n==k)
+    
+     
+     if(sb.toString().equals(x))
      System.out.println("yes");
      else
      System.out.println("no");
     }
-    public static int reverse(int n){
-        int k = 0;
-        int i=0;
-    while(n>0){
-        k = (k*10)+ (n%10);
-        n/=10;
-
-
-    }
-return k;
-
-    }
+  
     
 }
